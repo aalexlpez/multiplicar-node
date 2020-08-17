c07764c81068ae57f3de1b9d8dd51339e9c4718e
Alexander <alexander.lopez@kmg.com.ve>
2020-08-17 12:44:25 -0400

    Commit inicial

---
+++ app.js
@@ -0,0 +1,24 @@
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+const argv = require('./config/yargs').argv;
+var colors = require('colors');
+
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite)
+            .then()
+            .catch(e => console.log(e))
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${archivo}`))
+            .catch(e => console.log(e))
+        break;
+
+    default:
+        console.log('Comando no reconocido')
+        break;
+}
